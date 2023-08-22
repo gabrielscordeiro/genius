1820ead24099cdab2f5336f3ba263c4062509268
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2023-08-21 22:06:48 -0300

    feat: 🎸 Folter setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+    return (
+        <p>
+            Dashboard page
+        </p>
+    )
+}
+
+export default DashboardPage;
